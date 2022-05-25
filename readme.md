e908706b4e053079eed726d93250182f812da582
Vadim Starichkov <starichkovva@gmail.com>
2022-05-25 15:09:42 +0300

    Upgraded to Java 17 and Spring Boot 2.6

--- readme.md
+++ readme.md
@@ -1,11 +1,24 @@
 Spring Boot AOP examples
 =
+
+## Technical information
+
+| Piece of tech | Version |
+|---------------|---------|
+| Java          | 17      |
+| Spring Boot   | 2.6.x   |
+| Spring        | 5.3.x   |
+
+## Testing
+
 Two examples of how to log some method's parameter using aspects - before and after.
 
 Use SwaggerUI for testing these aspects:
+
 ```
 http://localhost:8080/swagger-ui.html
 ```
+
 Post some test object, and see the logs.
 
 Also added configurable by property `@Around` aspect to log execution time of the method.
--- src/main/java/org/starichkov/java/spring/aop/config/SwaggerConfig.java
+++ src/main/java/org/starichkov/java/spring/aop/config/SwaggerConfig.java
@@ -1,28 +1,24 @@
 package org.starichkov.java.spring.aop.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Vadim Starichkov
  * @since 20.11.2018 18:59
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI scimServiceOpenAPI(final BuildProperties buildProperties) {
+        return new OpenAPI()
+                .info(new Info().title("Spring Boot APO examples HTTP API")
+                        .description("Spring Boot APO examples HTTP API reference for the developers")
+                        .version(buildProperties.getVersion())
+                );
     }
-
 }
